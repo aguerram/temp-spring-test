3e38ca279c796e44a01d73a1307527e9f9412119
aguerram <agurram2013@gmail.com>
2022-03-01 15:37:17 +0100

    first

---
+++ src/main/java/aw/awssmtest/AwsSmTestApplication.java
@@ -0,0 +1,13 @@
+package aw.awssmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSmTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSmTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aw/awssmtest/AwsSmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package aw.awssmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSmTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
