e9a1fe6242cfdb5dd81d2ee0cc798372dc98be8c
aguerram <agurram2013@gmail.com>
2022-03-01 15:39:18 +0100

    first

--- src/main/java/aw/awssmtest/AwsSmTestApplication.java
+++ src/main/java/aw/awssmtest/AwsSmTestApplication.java
@@ -2,12 +2,21 @@ package aw.awssmtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AwsSmTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsSmTestApplication.class, args);
+	}
+
+
+	@GetMapping
+	public String hello(){
+		return "Hello World";
 	}
 
 }
